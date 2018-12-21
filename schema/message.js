d88da5086f8cf8dc88a3d223c4abcdda7c781bee
lyang9 <lyang9@buffalo.edu>
2018-12-21 00:26:14 -0500

    Added new files

---
+++ schema/message.js
@@ -0,0 +1,13 @@
+export default `
+
+  type Message {
+    id: Int!
+    text: String!
+    user: User!
+    channel: Channel!
+  }
+
+  type Mutation {
+    createMessage(channelId: Int!, text: String!): Boolean!
+  }
+`;
