0353fc1f0befa3b3c4a96ec693e67c646b207d64
Linda Yang <lindayang@Lindas-iMac.local>
2018-12-23 00:19:44 -0500

    Changed createUser to register and implemented hashed password

--- schema/user.js
+++ schema/user.js
@@ -16,6 +16,6 @@ export default `
     allUsers: [User!]!
   }
   type Mutation {
-    createUser(username: String!, email: String!, password: String!): User!
+    register(username: String!, email: String!, password: String!): Boolean!
   }
 `;
