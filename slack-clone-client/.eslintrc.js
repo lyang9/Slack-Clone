21e3c25ee6d20b4ace2811ddbd2eb76c0167716a
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-01-07 01:16:03 -0500

    Completed eslintrc.js

--- slack-clone-client/.eslintrc.js
+++ slack-clone-client/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+  globals: {
+    document: 1,
+  },
 };
