ffff31c1440b9771ae9b78b40246aa6c27b81d57
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-01-07 01:22:53 -0500

    Imported routes and dependencies from react-apollo

--- slack-clone-client/src/index.js
+++ slack-clone-client/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
+
+import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8081/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
